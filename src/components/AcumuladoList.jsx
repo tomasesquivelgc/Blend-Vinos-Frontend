07819dff8d231d046dd78ab5f7c5ba65bc71df5c
Tomas Esquivel <tomasesquivel@yahoo.com>
2025-11-14 11:29:58 -0300

    Added Cepa and añejamiento to acumulados

--- src/components/AcumuladoList.jsx
+++ src/components/AcumuladoList.jsx
@@ -36,11 +36,13 @@ export default function AcumuladoList() {
       {!loading && !error && (
         <ul className="divide-y-2 divide-gray-400 border border-gray-400 bg-white">
           {items.map((w) => (
-            <li key={w.id || w._id || w.codigo} className="p-4 bg-white h-[120px]">
+            <li key={w.id || w._id || w.codigo} className="p-4 bg-white md:h-[120px]">
               <div className="text-lg font-semibold">{w.nombre || w.name || 'Sin nombre'}</div>
-              <div className="flex justify-between h-full">
+              <div className="flex flex-col md:flex-row justify-between h-fit">
                 <div>
                   <div className="text-sm text-gray-700">{w.codigo || w.varietal || ''}</div>
+                  <div className='text-sm text-gray-700'>{w.cepa || ""}</div>
+                  <div className="text-sm text-gray-700">{w.anejamiento || ''}</div>
                 </div>
                 <div>
                   <div className="text-sm text-gray-700">Total: <span className="font-semibold">{w.total ?? '-'}</span></div>
